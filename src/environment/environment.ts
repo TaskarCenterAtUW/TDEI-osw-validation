100d3c2ac671aaf721eae0e6658bfb4e2cbf314f
Naresh Kumar D <susrisha@gmail.com>
2023-04-27 12:48:02 +0530

    optimized environment variables
    changed the environment variables.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -9,12 +9,12 @@ dotenv.config();
 export const environment = {
     appName: process.env.npm_package_name,
     eventBus: {
-        connectionString: process.env.EVENT_BUS_CONNECTION,
+        connectionString: process.env.QUEUECONNECTION,
         uploadTopic: process.env.UPLOAD_TOPIC,
         uploadSubscription: process.env.UPLOAD_SUBSCRIPTION,
         validationTopic: process.env.VALIDATION_TOPIC
     },
-    appPort: parseInt(process.env.APPLICATION_PORT ?? "8080"),
+    appPort: parseInt(process.env.PORT ?? "8080"),
     authPermissionUrl: process.env.AUTH_PERMISSION_URL,
     authProvider: <ServiceProvider>(process.env.AUTH_SIMULATE ? "Simulated" : "Hosted")
 }
